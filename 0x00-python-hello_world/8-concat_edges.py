f06305e10a3b541df0520c2eac3b6aba50a73809
omaryahia4 <omar.yahia4444@gmail.com>
2021-04-26 22:48:12 +0100

    choosing words to print

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[106:112] + str[:6]
+str = "object-oriented programming with Python"
 print(str)
