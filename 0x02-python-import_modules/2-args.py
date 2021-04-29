4556768747ad2c7e7aa0089bad2d089642dfccc5
omaryahia4 <omar.yahia4444@gmail.com>
2021-04-29 14:47:57 +0100

    play with arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 0:
+        print("{:d} arguments".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 1:
+        print("{:d} argument".format(len(sys.argv) - 1))
+    else:
+        print("{:d} arguments".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{:d}: {}".format(i, sys.argv[i]))
