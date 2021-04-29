0d4df410f661435bddef3d1c4789df586ec03547
omaryahia4 <omar.yahia4444@gmail.com>
2021-04-29 14:54:27 +0100

    play with arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 0:
+    if len(sys.argv) - 1 == 0:
         print("{:d} arguments".format(len(sys.argv)))
     elif len(sys.argv) - 1 == 1:
         print("{:d} argument".format(len(sys.argv) - 1))
