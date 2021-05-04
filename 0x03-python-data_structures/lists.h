16075077d7af6c938ceca456231b9aa0e0279aeb
omaryahia4 <omar.yahia4444@gmail.com>
2021-05-04 14:08:44 +0100

    header file

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,17 @@
+def print_list_integer(my_list=[]):
+def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
+def print_reversed_list_integer(my_list=[]):
+def new_in_list(my_list, idx, element):
+def no_c(my_string):
+def print_matrix_integer(matrix=[[]]):
+def add_tuple(tuple_a=(), tuple_b=()):
+def multiple_returns(sentence):
+def max_integer(my_list=[]):
+def divisible_by_2(my_list=[]):
+def delete_at(my_list=[], idx=0):
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int is_palindrome(listint_t **head);
+void print_python_list_info(PyObject *p);
