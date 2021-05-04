1f7cad770efe62e6d80393b980c7a73d648fdb47
omaryahia4 <omar.yahia4444@gmail.com>
2021-05-05 00:02:44 +0100

    delete index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,6 +3,6 @@ def delete_at(my_list=[], idx=0):
     for i in list(my_list):
         if idx < 0 or idx >= len(my_list):
             return my_list
-        else:
+        elif idx == i:
             my_list.remove(idx + 1)
-        return my_list
+    return my_list
