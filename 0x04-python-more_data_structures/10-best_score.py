8d8a6dbc2174b608148f530b0f3ed89cf1974cab
omaryahia4 <omar.yahia4444@gmail.com>
2021-05-06 20:57:46 +0000

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        key = max(a_dictionary, key = lambda k: a_dictionary[k])
+        return key
