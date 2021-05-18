d900b512e64a004f5a4ad122bd6dedaf153091d5
omaryahia4 <omar.yahia4444@gmail.com>
2021-05-18 21:02:38 +0000

    print #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module Square"""
+
+
+class Square:
+    """ Square class """
+    def __init__(self, size=0):
+        """Initializes the data
+
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
+
+    def area(self):
+        """The square's square area"""
+        self.area = self.__size ** 2
+        return int(self.area)
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
