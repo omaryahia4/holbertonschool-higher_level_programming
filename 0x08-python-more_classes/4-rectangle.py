3db2c614332cc675e5cbfd1860d2f7ea973e8313
omaryahia4 <omar.yahia4444@gmail.com>
2021-05-24 20:24:39 +0000

    remove whitespace

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -105,4 +105,4 @@ class Rectangle:
             Return:
             (str) representation of the rectangle
         """
-        return("Rectangle ({}, {})".format(self.__width, self.__height))
+        return("Rectangle({}, {})".format(self.__width, self.__height))
