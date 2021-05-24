7f0c295d7136d980cd8e304f3373d18c51d9c975
omaryahia4 <omar.yahia4444@gmail.com>
2021-05-24 20:49:05 +0000

    code fix

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -105,7 +105,7 @@ class Rectangle:
             Return:
             (str) representation of the rectangle
         """
-        return("Rectangle ({}, {})".format(self.__width, self.__height))
+        return("Rectangle({}, {})".format(self.__width, self.__height))
 
     def __del__(self):
-        print ("Bye rectangle...")
+        print("Bye rectangle...")
