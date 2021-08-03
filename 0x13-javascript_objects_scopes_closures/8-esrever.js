d80d085ccc2bc9f4ae9f35425023e16f41650d89
omaryahia4 <omar.yahia4444@gmail.com>
2021-08-03 22:19:58 +0000

    fix code

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-exports.esrever = function (list) {
+export function esrever (list) {
     let i;
     const rev = [];
     for (i = list.length - 1; i >= 0; i--) {
       rev.push(list[i]);
     }
     return rev;
-  };
+  }
