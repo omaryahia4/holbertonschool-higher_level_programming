41c8b42950332fbde721bd3d8d1a41989645a100
omaryahia4 <omar.yahia4444@gmail.com>
2021-08-07 15:41:50 +0100

    arizona

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 ''' lists all states from the database hbtn_0e_0_usa '''
-if __name__ == "__main__":
+
+
+def list_all_states():
     import MySQLdb
     import sys
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states  ORDER BY id ASC".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states  ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if (row[1] == sys.argv[4]):
+            print(row)
     cur.close()
     conn.close()
+
+
+if __name__ == "__main__":
+    list_all_states()
