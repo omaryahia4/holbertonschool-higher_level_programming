c4515f73e89d04baf7229045f0369fc6c3034321
omaryahia4 <omar.yahia4444@gmail.com>
2021-08-08 16:23:15 +0100

    letter a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" lists all States with first letter 'a' from the database hbtn_0e_6_usa"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+import model_state
+from model_state import Base, State
+import sys
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    query_row = session.query(State).filter(
+                              State.name.like('%a%')).order_by(State.id)
+    for i in query_row:
+        print("{}: {}".format(i.id, i.name))
+    session.close()
