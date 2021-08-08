5d6efb2d79f3cb4354598a469eb0dba60d8c3833
omaryahia4 <omar.yahia4444@gmail.com>
2021-08-08 16:34:34 +0100

    state id

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" lists all State objects from the database hbtn_0e_6_usa"""
+""" lists the state id of entered argument from the database hbtn_0e_6_usa"""
 
 
 from sqlalchemy import create_engine
