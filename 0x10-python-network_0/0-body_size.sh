64e19db5fadaa2323ef5c0bb78e5241151c43862
omaryahia4 <omar.yahia4444@gmail.com>
2021-08-19 14:20:09 +0100

    cURL

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Bash script that takes in a URL, sends a request to that URL, 
+# and displays the size of the body of the response
+ curl -sI $URL | grep -i Content-Length | awk '{print $2}'
