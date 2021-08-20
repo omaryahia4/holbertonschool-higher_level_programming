18d99a75bcd100b48d468c690a2c3bc42e5df948
omaryahia4 <omar.yahia4444@gmail.com>
2021-08-20 17:50:56 +0100

    header with request

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""sends a request to the URL and displays the value 
+"""sends a request to the URL and displays the value
 of the variable X-Request-Id in the response header"""
 import requests
 import sys
