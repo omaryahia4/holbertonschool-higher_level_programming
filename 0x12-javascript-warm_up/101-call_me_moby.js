c8751b8bac1f7431fbf26cb50ce41a44a39a71c3
omaryahia4 <omar.yahia4444@gmail.com>
2021-09-13 15:38:00 +0100

    function

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-export function callMeMoby (num, theFunction) {
-    for (let i = 0; i < num; i++) {
+exports.callMeMoby = function (x, theFunction) {
+    for (let i = 0; i < x; i++) {
       theFunction();
     }
-  }
+  };
