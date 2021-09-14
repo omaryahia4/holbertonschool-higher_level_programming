d79055bd01012dd786dcbf0b768c281bb830018a
omaryahia4 <omar.yahia4444@gmail.com>
2021-09-14 21:26:41 +0100

    contents of a webpage

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const request = require('request');
+const req = require('request');
 const fs = require('fs');
 const url = process.argv[2];
 const filename = process.argv[3];
-request(url, function (errr, response, body) {
+req(url, function (err, response, body) {
   fs.writeFile(filename, body, 'utf8', function (err, data) {
     if (err) {
       console.log(err);
