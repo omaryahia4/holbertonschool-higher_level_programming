98df61e74c54dc5a084785c25396a6ddb34f1b8d
omaryahia4 <omar.yahia4444@gmail.com>
2021-09-14 21:23:45 +0100

    contents of a webpage

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,9 +6,9 @@ const filename = process.argv[3];
 request(url, function (errr, response, body) {
   fs.writeFile(filename, body, 'utf8', function (err, data) {
     if (err) {
-        console.log(err);
-      } else {
-        console.log(data);
-      }
+      console.log(err);
+    } else {
+      console.log(data);
+    }
   });
 });
