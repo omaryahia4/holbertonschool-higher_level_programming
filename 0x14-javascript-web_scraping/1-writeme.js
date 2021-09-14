9ee7200df3d0ceb10d957c7080d4a2c3c99ff3f1
omaryahia4 <omar.yahia4444@gmail.com>
2021-09-14 17:54:09 +0100

    write into file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(data);
-    }
+  if (err) throw err;
 });
