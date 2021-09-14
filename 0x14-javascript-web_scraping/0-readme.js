65408546673dc862bfbc5fd03b384aa53c8aea19
omaryahia4 <omar.yahia4444@gmail.com>
2021-09-14 17:43:02 +0100

    read file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
-    if (err) {
-        console.log(err);
+  if (err) {
+    console.log(err);
       } else {
         console.log(data);
       }
