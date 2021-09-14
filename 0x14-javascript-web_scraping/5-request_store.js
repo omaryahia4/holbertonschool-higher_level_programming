ad1412852defa6af096f42c19dbb267caf2a728b
omaryahia4 <omar.yahia4444@gmail.com>
2021-09-14 22:00:17 +0100

    fixed

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
-const req = require('request');
+const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
 const filename = process.argv[3];
-req(url, function (err, response, body) {
-  fs.writeFile(filename, body, 'utf8', function (err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(data);
-    }
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  fs.writeFile(filename, body, 'utf8', (err) => {
+    if (err) console.log(err);
   });
 });
